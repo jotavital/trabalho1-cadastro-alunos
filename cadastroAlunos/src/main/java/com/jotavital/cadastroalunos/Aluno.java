8baff384d56836b67976f6ad989dcb3b6cc0f50e
jotavital <joaopedrovitaldoctum@gmail.com>
2021-03-21 00:14:24 -0300

    Renamed project and folders
    renomeei os projetos e pastas para melhor localizacao dos arquivos

--- cadastroAlunos/src/main/java/com/jotavital/cadastroalunos/Aluno.java
+++ cadastroAlunos/src/main/java/com/jotavital/cadastroalunos/Aluno.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.jotavital.projeto.cadastro.alunos;
+package com.jotavital.cadastroalunos;
 
 import java.util.ArrayList;
 
